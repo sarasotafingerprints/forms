3240d5d98ae17c81c711c6e222c23d35b7c99901
sarasotafingerprints <mike@sarasotafingerprints.com>
2022-12-27 11:22:24 -0500

    added data-tf-inline-on-mobile

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import { Widget } from '@typeform/embed-react'
 
 export default function Home() {
   return (
-    <Widget id="StIuBpvq" style={{ width: '100%', height: '100vh' }} className="my-form" />
+    <Widget id="StIuBpvq" style={{ width: '100%', height: '100vh' }} className="my-form" data-tf-inline-on-mobile/>
   )
 }
