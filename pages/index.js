14223f7c116645c9869d6c6ca68cefdc23c2dbe3
sarasotafingerprints <mike@sarasotafingerprints.com>
2023-09-05 11:53:23 -0400

    added question changed callback
    added question changed callback

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,11 @@ import { Widget } from '@typeform/embed-react'
 
 export default function Home() {
   return (
-    <Widget id="StIuBpvq" style={{ width: '100%', height: '85vh' }} className="my-form" inlineOnMobile={false} hidden={{ kiosk: '1'}}/>
+    <Widget id="StIuBpvq" style={{ width: '100%', height: '85vh' }} className="my-form" inlineOnMobile={false} 
+    hidden={{ kiosk: '1'}}
+    onQuestionChanged={(formId, ref) => {
+      console.log(`Question in form ${formId} changed to ${ref}`)
+    }}
+    />
   )
 }
