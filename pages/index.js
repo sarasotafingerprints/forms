36452e00d72297ac1e6d892cd4d1b05f712cedba
sarasotafingerprints <mike@sarasotafingerprints.com>
2023-09-05 10:41:55 -0400

    85 vh

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import { Widget } from '@typeform/embed-react'
 
 export default function Home() {
   return (
-    <Widget id="StIuBpvq" style={{ width: '100%', height: '90vh' }} className="my-form" inlineOnMobile={false} hidden={{ kiosk: '1'}}/>
+    <Widget id="StIuBpvq" style={{ width: '100%', height: '85vh' }} className="my-form" inlineOnMobile={false} hidden={{ kiosk: '1'}}/>
   )
 }
